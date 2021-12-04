05af6ad0ee9ac0e7aa7930dd79bc9619daf2953f
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-04 15:46:05 +0530

    [Akshay]ADDED: code snippet explaining var, const and let.

--- variables.js
+++ variables.js
@@ -5,3 +5,55 @@ var car2 = "dodge-hellcart";
 
 console.log(car1);
 console.log(car2);
+
+
+
+// hoisting
+
+
+//this code prints undefined
+console.log(a);
+var a = 10;
+console.log(a);
+
+
+//code leads to error
+// console.log(b);
+let b = 20;
+console.log(b)
+
+
+
+//scope demonstration
+
+var temp1 = 'hello ppl';
+
+if(true){
+    var temp1 = 'second time defined'; //prints second time defined
+    console.log(temp1);
+}
+
+console.log(temp1); //prints second time defined
+
+
+let temp2 = 'hello ppl';
+
+if(true){
+    let temp2 = 'second time defined'; // prints second time defined
+    console.log(temp2);
+}
+
+console.log(temp2);  //prints hello ppl
+
+
+
+// const key word is block scoped and used to define constants
+
+const c = 100;
+
+console.log(c);
+
+
+// console.log(c1); gives error
+const c1 = 244;
+console.log(c1);
